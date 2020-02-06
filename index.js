5523f4237333f42851e7f596ae7771f09ec32c1c
AlannJoy <alldaubeterre@gmail.com>
2020-02-06 18:05:14 +0100

    Add db.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const port = process.env.PORT || 4000;
 const app = express();
+const db = require("./db");
 
 app.get("/test", (req, res) => {
   console.log("Hello World");
